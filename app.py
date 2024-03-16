9fb867bffc3910d6929a29c2d6b30cfb65edd000
K1REBEL <kibraheem47@yahoo.com>
2024-03-16 14:18:53 +0200

    Scrapping Successful

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import json
 from bs4 import BeautifulSoup
+from chromedriver_py import binary_path
 from selenium import webdriver
 from flask import Flask, request, jsonify
 
@@ -149,6 +150,20 @@ def btech():
 @app.route('/scrape', methods=['GET'])
 def scrape():
    try:
+
+      # chrome_options = Options()
+      chrome_options = webdriver.ChromeOptions()
+      # chrome_options.binary_location = '/opt/google/chrome/google-chrome'
+      chrome_options.add_argument("--headless")
+      chrome_options.add_argument("--remote-debugging-port=9222")
+
+      s = webdriver.ChromeService(executable_path=binary_path)
+      driver = webdriver.Chrome(service=s, options=chrome_options)
+      # driver = webdriver.Chrome(executable_path="./.venv/bin/chromedriver.exe", options=chrome_options)
+      # driver = webdriver.Chrome(executable_path="./.venv/bin/chromedriver.exe")
+
+      # Initialize an empty list to store scraped data
+      scraped_data = []
       data = request.get_json()
 
       if not isinstance(data, list):
@@ -162,13 +177,36 @@ def scrape():
 
       if len(asins) != len(urls):
          return jsonify({'error': 'Number of ASINs must match the number of URLs'}), 400
+      
+      for url in urls:
+         driver.get(url)  # Open the URL in the browser
+         html_content = driver.page_source  # Get the HTML content
 
+         # Parse the HTML content using Beautiful Soup
+         soup = BeautifulSoup(html_content, "html.parser")
+
+         # Extract relevant information from the page (customize this part)
+         # Example: Get the title
+         title = soup.find("title").text.strip()
+
+         # Append the scraped data to the list
+         scraped_data.append({"url": url, "title": title})
+
+         # Save the scraped data to a JSON file
+         with open("scraped_data.json", "w") as json_file:
+            json.dump(scraped_data, json_file, indent=3)
+
+         
+
+         # return jsonify({"message": "Data scraped successfully!"})
+      
+      driver.quit()  # Close the browser
       scrape_data = [{'asin': asin, 'url': url} for asin, url in zip(asins, urls)]
 
       with open('amazon.json', 'w') as f:
          json.dump(scrape_data, f, indent=3)
 
-      return jsonify({'message': 'Amazon Data Received!'})
+      return jsonify({"message": "Data scraped successfully!"})
 
    except Exception as e:
       return jsonify({'error': str(e)}), 500
